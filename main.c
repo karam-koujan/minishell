1d069c38fc3e52f063e2e9cdfdeab216b04d9d63
Karam Koujan <kkoujan@e2r11p1.1337.ma>
2025-03-06 03:39:45 +0000

    chore: add has_misplaced_pipes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 14:08:27 by kkoujan           #+#    #+#             */
-/*   Updated: 2025/03/06 02:56:02 by kkoujan          ###   ########.fr       */
+/*   Updated: 2025/03/06 03:30:25 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,41 @@ int has_unclosed_quotes(char    *cmd)
     return (!(is_double_quote || is_single_quote));
 }
 
+
+int is_space(char    c)
+{
+    return ((c >= 9 && c <= 13) || c == 32);
+}
+
+int has_misplaced_pipes(char    *cmd)
+{
+    int i;
+    int j;
+    int pipe;
+
+    i = 0;
+    j = ft_strlen(cmd) - 1;
+    pipe = 0;
+    while (cmd[i] && is_space(cmd[i]))
+        i++;
+    while (j >= 0 && is_space(cmd[i]))
+        j--;
+    if (cmd[i] == '|' || cmd[j] == '|')
+        return (0);
+    while (cmd[i])
+    {
+        if (cmd[i] == '|' && pipe == 1)
+            return (0);
+        if (cmd[i] == '|' && pipe == 0)
+            pipe = 1;
+        if (cmd[i] != '|' && !is_space(cmd[i]))
+            pipe = 0;
+        i++;
+    }
+    return (1);
+}
+
+
 int main()
 {
     char    *line;
@@ -45,6 +80,8 @@ int main()
         return (0);
     printf("line %s\n", line);
     printf("line %i\n", has_unclosed_quotes(line));
+    printf("line %i\n", has_misplaced_pipes(line));
+
     free(line);
     }
 }
