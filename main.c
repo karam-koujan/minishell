5c0b8d8cc590a743e80afd9f31a09503a812fb30
Karam Koujan <kkoujan@e2r10p1.1337.ma>
2025-03-07 06:57:45 +0000

    feat: add syntax checker function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 14:08:27 by kkoujan           #+#    #+#             */
-/*   Updated: 2025/03/06 03:52:17 by kkoujan          ###   ########.fr       */
+/*   Updated: 2025/03/07 06:50:52 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,39 +56,45 @@ int	is_operator(char c)
 	return (0);
 }
 
-int	is_unvalid_arg(char c)
-{
-	return (is_space(c) || is_operator(c));
-}
-
 int	has_misplaced_pipes(char *cmd)
 {
 	int	i;
 	int	j;
 	int	pipe;
+	int	is_double_quote;
+	int	is_single_quote;
 
 	i = 0;
 	j = ft_strlen(cmd) - 1;
 	pipe = 0;
+	is_double_quote = 0;
+	is_single_quote = 0;
 	while (cmd[i] && is_space(cmd[i]))
 		i++;
 	while (j >= 0 && is_space(cmd[j]))
 		j--;
-	if (cmd[i] == '|' || cmd[j] == '|')
-		return (1);
+	if (cmd[i] == '|')
+		return ((int)cmd[i]);
+	if (cmd[j] == '|')
+		return ((int)cmd[j]);
 	while (cmd[i])
 	{
-		if (cmd[i] == '\'' || cmd[i] == '"')
-			break ;
-		if (cmd[i] == '|' && pipe == 1)
-			return (1);
-		if (cmd[i] == '|' && pipe == 0)
-			pipe = 1;
-		if (ft_strchr("<>", cmd[i]) != NULL && pipe != 0)
-			return (1);
-		if (cmd[i] != '|' && !is_space(cmd[i]) && \
-		ft_strchr("<>", cmd[i]) == NULL)
-			pipe = 0;
+		if (cmd[i] == '\'' && is_double_quote == 0)
+			is_single_quote = !is_single_quote;
+		else if (cmd[i] == '"' && is_single_quote == 0)
+			is_double_quote = !is_double_quote;
+		if (!is_single_quote && !is_double_quote)
+		{
+			if (cmd[i] == '|' && pipe == 1)
+				return ((int)cmd[i]);
+			if (cmd[i] == '|' && pipe == 0)
+				pipe = 1;
+			if (ft_strchr("<>", cmd[i]) != NULL && pipe != 0)
+				return ((int)cmd[i]);
+			if (cmd[i] != '|' && !is_space(cmd[i]) && \
+				ft_strchr("<>", cmd[i]) == NULL)
+				pipe = 0;
+		}
 		i++;
 	}
 	return (0);
@@ -100,59 +106,115 @@ int	has_invalid_redir(char *cmd)
 	int	j;
 	int	redirfor;
 	int	redirback;
+	int	is_double_quote;
+	int	is_single_quote;
 
 	i = 0;
 	j = ft_strlen(cmd) - 1;
 	redirfor = 0;
 	redirback = 0;
+	is_double_quote = 0;
+	is_single_quote = 0;
 	while (cmd[i] && is_space(cmd[i]))
 		i++;
 	while (j >= 0 && is_space(cmd[j]))
 		j--;
-	if (cmd[i] == '<' || cmd[i] == '>' || cmd[j] == '<' || cmd[j] == '>')
-		return (1);
+	if (cmd[i] == '<' || cmd[i] == '>')
+		return ((int)cmd[i]);
+	if ( cmd[j] == '<' || cmd[j] == '>')
+		return ((int)cmd[j]);
 	while (cmd[i])
 	{
-		if (cmd[i] == '\'' || cmd[i] == '"')
-			break ;
-		if (cmd[i] == '>' && (redirfor > 2 || redirback != 0))
-			return (1);
-		if (i > 0 && cmd[i] == '>' && is_space(cmd[i-1])  && redirfor > 0)
-			return (1);
-		if (i > 0 && cmd[i] == '<' && is_space(cmd[i-1]) && redirback > 0)
-			return (1);
-		if (ft_strchr("|", cmd[i]) != NULL && (redirfor != 0 || redirback != 0))
-			return (1);
-		if (cmd[i] == '<' && (redirback > 2 || redirfor != 0))
-			return (1);
-		if (cmd[i] == '>' && (redirfor < 2 || redirback == 0))
-			redirfor++;
-		if (cmd[i] == '<' && (redirback < 2 || redirfor == 0))
-			redirback++;
-		if ((cmd[i] != '<' && cmd[i] != '>') && !is_space(cmd[i]) && \
-		ft_strchr("|", cmd[i]) == NULL)
+		if (cmd[i] == '\'' && is_double_quote == 0)
+			is_single_quote = !is_single_quote;
+		else if (cmd[i] == '"' && is_single_quote == 0)
+			is_double_quote = !is_double_quote;
+		if (!is_single_quote && !is_double_quote)
 		{
-			redirback = 0;
-			redirfor = 0;
+			if (cmd[i] == '>' && (redirfor >= 2 || redirback != 0))
+				return ((int)cmd[i]);
+			if (i > 0 && cmd[i] == '>' && is_space(cmd[i-1])  && redirfor > 0)
+				return ((int)cmd[i]);
+			if (i > 0 && cmd[i] == '<' && is_space(cmd[i-1]) && redirback > 0)
+				return ((int)cmd[i]);
+			if (ft_strchr("|", cmd[i]) != NULL && (redirfor != 0 \
+			|| redirback != 0))
+				return ((int)cmd[i]);
+			if (cmd[i] == '<' && (redirback >= 2 || redirfor != 0))
+				return ((int)cmd[i]);
+			if (cmd[i] == '>' && (redirfor < 2 || redirback == 0))
+				redirfor++;
+			if (cmd[i] == '<' && (redirback < 2 || redirfor == 0))
+				redirback++;
+			if ((cmd[i] != '<' && cmd[i] != '>') && !is_space(cmd[i]) && \
+			ft_strchr("|", cmd[i]) == NULL)
+			{
+				redirback = 0;
+				redirfor = 0;
+			}
 		}
 		i++;
 	}
 	return (0);
 }
 
+int	has_logical_op(char	*cmd)
+{
+	size_t	i;
+	int		is_single_quote;
+	int		is_double_quote;
+
+	i = 0;
+	while (cmd[i])
+	{
+		if (cmd[i] == '\'' && is_double_quote != 0)
+			is_single_quote = !is_single_quote;
+		if (cmd[i] == '"' && is_single_quote != 0)
+			is_double_quote = !is_double_quote;
+		if (!is_double_quote && !is_single_quote)
+		{
+			if (i + 1 < ft_strlen(cmd) && cmd[i] == '|' && cmd[i + 1] == '|')
+				return ((int)cmd[i]);
+			if (i + 1 < ft_strlen(cmd) && cmd[i] == '&' && cmd[i + 1] == '&')
+				return ((int)cmd[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+int	syntax_error(char	*cmd)
+{
+	char	*unclosed_quote_err;
+	char	*log_op_err;
+	char	*operator_err;
+	int		operator;
+
+	unclosed_quote_err = "minishell: syntax error: unclosed quote\n";
+	operator_err = "minishell: syntax error near unexpected token ";
+	log_op_err = "minishell: syntax error : unsupported operator ";
+	operator = has_invalid_redir(cmd);
+	if (has_unclosed_quotes(cmd))
+		return (write(2, unclosed_quote_err, ft_strlen(unclosed_quote_err)), 1);
+	if (operator && write(2, operator_err, ft_strlen(operator_err)))
+		return (write(2, &operator, 1), write(2,"\n",1), 1);
+	operator = has_misplaced_pipes(cmd);
+	if (operator && write(2, operator_err, ft_strlen(operator_err)))
+		return (write(2, &operator, 1), write(2,"\n",1), 1);
+	operator = has_logical_op(cmd);
+	if (operator && write(2, log_op_err, ft_strlen(log_op_err)))
+		return (write(2, &operator, 1), write(2, &operator, 1), write(2,"\n",1), 1);
+	return (0);
+}
 int main()
 {
-	char    *line;
+	char    *cmd;
 	while (1337)
 	{
-	line = readline("minishell$ ");
-	if (!line)
+	cmd = readline("minishell$ ");
+	if (!cmd && !*cmd)
 		return (0);
-	printf("line %s\n", line);
-	printf("line %i\n", has_unclosed_quotes(line));
-	printf("line %i\n", has_misplaced_pipes(line));
-	printf("redir %i\n", has_invalid_redir(line));
-
-	free(line);
+	syntax_error(cmd);
+	add_history(cmd);
+	free(cmd);
 	}
 }
