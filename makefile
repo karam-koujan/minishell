2ff6d7b96746b0fab7fb463e9cc4dcb1dbaf9ef7
Karam Koujan <kkoujan@e1r8p7.1337.ma>
2025-03-16 01:36:42 +0000

    untested: tokenize commands

--- makefile
+++ makefile
@@ -1,14 +1,15 @@
 Name = minishell
-CC = cc 
-CFLAGS = -Wall -Wextra -Werror -lreadline
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
 OBJ = main.o ./libft/ft_strlen.o ./libft/ft_strchr.o ./libft/ft_strnstr.o  src/error_checker/error_checker.o \
 src/error_checker/error_checker_helper.o ./libft/ft_memcpy.o libft/ft_lstadd_back_bonus.o src/tokenizer/tokenizer.o \
-./libft/ft_strdup.o ./libft/ft_strlcpy.o
+./libft/ft_strdup.o ./libft/ft_strlcpy.o ./libft/ft_substr.o ./libft/ft_strncmp.o ./libft/ft_isalnum.o ./libft/ft_isdigit.o \
+./libft/ft_isalpha.o
 
 all : $(Name)
 
 $(Name) : $(OBJ)
-	$(CC) $(CFLAGS) $^  -o $(Name)
+	$(CC) $(CFLAGS) -lreadline $^  -o $(Name)
 
 
 %.o : %.c libft/libft.h includes/error_checker.h includes/minishell.H
@@ -21,4 +22,4 @@ clean :
 fclean : clean
 	rm -f $(Name)
 
-re : fclean all 
+re : fclean all
--- src/tokenizer/tokenizer.c
+++ src/tokenizer/tokenizer.c
@@ -6,11 +6,25 @@
 /*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 23:42:20 by kkoujan           #+#    #+#             */
-/*   Updated: 2025/03/14 01:43:51 by kkoujan          ###   ########.fr       */
+/*   Updated: 2025/03/16 01:34:23 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/tokenizer.h"
+
+int	is_escaped(char *str)
+{
+	int	count;
+
+	count = 0;
+	str = str - 1;
+	while (*str == '\\')
+	{
+		str = str - 1;
+		count++;
+	}
+	return (count % 2 != 0);
+}
 
 char	*quote_token(char **cmd, char end_char)
 {
@@ -21,7 +35,8 @@ char	*quote_token(char **cmd, char end_char)
 	count = 0;
 	start = *cmd;
 
-	while (**cmd && **cmd != end_char)
+	while (**cmd && (**cmd != end_char || \
+		(**cmd == end_char && is_escaped(*cmd))))
 	{
 		(*cmd)++;
 		count++;
@@ -100,80 +115,59 @@ void	ft_token_add_back(t_token **lst, t_token *new)
 	}
 }
 
+int	add_token(t_token **head, char *start_ptr, int len, t_token_type type)
+{
+	char	*token;
+	t_token	*node_token;
+
+	token = ft_substr(start_ptr, 0, len);
+	if (!token)
+		return (free(token), 0);
+	node_token = init_token(type, token);
+	if (!node_token)
+		return (free(token), 0);
+	ft_token_add_back(head, node_token);
+	return (1);
+}
+
+
+
 int	var_quote_token(char **str, t_token **head)
 {
-	int		j;
-	char	*token;
-	char	buffer[256];
-	t_token	*node_token;
+	int		len;
 	int		is_var;
+	char	*start_pos;
 
-	j = 0;
+	len = 0;
 	is_var = 0;
+	start_pos = *str;
 	while (**str)
 	{
-		if (**str == '$')
+		if (**str == '$' && !ft_isalnum(*(*str + 1)))
+			len++;
+		else if (**str == '$' && ft_isalnum(*(*str + 1)))
 		{
-			if ((*(*str + 1) == '\0' || is_whitespace(*(*str + 1)) || \
-			*(*str + 1) == '"'))
-			{
-				buffer[j] = **str;
-				j++;
-				(*str)++;
-				continue ;
-			}
-			buffer[j] = 0;
-			if (j > 0)
-			{
-				token = ft_strdup(buffer);
-				if (!token)
-					return (free(token), 0);
-				if (is_var)
-					node_token = init_token(VAR_T, token);
-				else
-					node_token = init_token(ARG_T, token);
-				if (!node_token)
-					return (free(token), 0);
-				ft_token_add_back(head, node_token);
-				j = 0;
-				is_var = 1;
-			}
+			is_var = 1;
+			if (add_token(head, start_pos, len, ARG_T) == 0)
+				return (0);
+			start_pos = *str + 1;
+			len = 0;
 		}
-		else if (**str == '"')
+		else if (**str == '"' && !is_escaped(*str))
 		{
-			buffer[j] = 0;
-			token = ft_strdup(buffer);
-			if (!token)
-				return (free(token), 0);
 			if (is_var)
-				node_token = init_token(VAR_T, token);
-			else
-				node_token = init_token(ARG_T, token);
-			if (!node_token)
-				return (free(token), 0);
-			ft_token_add_back(head, node_token);
-			j = 0;
-			(*str)++;
-			return (1);
+				return (add_token(head, start_pos, len, VAR_T));
+			return (add_token(head, start_pos, len, ARG_T));
 		}
 		else if (is_whitespace(**str) && is_var)
 		{
-			buffer[j] = 0;
-			token = ft_strdup(buffer);
-			if (!token)
-				return (free(token), 0);
-			node_token = init_token(VAR_T, token);
+			add_token(head, start_pos, len, VAR_T);
+			len = 1;
 			is_var = 0;
-			if (!node_token)
-				return (free(token), 0);
-			ft_token_add_back(head, node_token);
-			j = 0;
+			start_pos = *str;
 		}
 		else
-		{
-			buffer[j] = **str;
-			j++;
-		}
+			len++;
 		(*str)++;
 	}
 	return (1);
@@ -181,20 +175,32 @@ int	var_quote_token(char **str, t_token **head)
 
 t_token	*tokenize(char *cmd)
 {
-	int		i;
 	char	*token;
 	t_token	*head;
 	t_token	*node_token;
-	char	*current_pos;
+	char	*start_pos;
+	int		len;
 
-	i = 0;
+	len = 0;
+	start_pos = cmd;
 	head = NULL;
-	while (cmd[i])
+	while (*cmd)
 	{
-		if (cmd[i] == '\'')
+		if (is_whitespace(*cmd))
 		{
-			current_pos = cmd + i + 1;
-			token = quote_token(&current_pos, '\'');
+			if (head == NULL && len > 0)
+				add_token(&head, start_pos, len, CMD_T);
+			else if (head != NULL && len > 0)
+				add_token(&head, start_pos, len, ARG_T);
+
+			cmd++;
+			len = 0;
+			start_pos = cmd;
+			continue ;
+		}
+		if (*cmd == '\'' && !is_escaped(cmd))
+		{
+			token = quote_token(&cmd, '\'');
 			if (!token)
 				return (free(token), NULL);
 			node_token = init_token(ARG_T, token);
@@ -202,56 +208,86 @@ t_token	*tokenize(char *cmd)
 			if (!node_token)
 				return (free(token), NULL);
 			ft_token_add_back((t_token **)&head, (t_token *)node_token);
-			i = current_pos - cmd;
+			cmd++;
+			len = 0;
+			start_pos = cmd;
+			continue ;
 		}
-		if (cmd[i] == '"')
+		if (*cmd == '"' && !is_escaped(cmd))
 		{
-			current_pos = cmd + i + 1;
-			if (!var_quote_token(&current_pos, &head))
+			cmd++;
+			if (!var_quote_token(&cmd, &head))
 				return (NULL);
-			i = current_pos - cmd;
+			cmd++;
+			len = 0;
+			start_pos = cmd;
+			continue ;
 		}
-		if (cmd[i] == '$')
+		if (*cmd == '$')
 		{
-			current_pos = cmd + i + 1;
-			token = var_token(&current_pos);
+			cmd++;
+			token = var_token(&cmd);
 			if (!token)
 				return (free(token), NULL);
 			node_token = init_token(VAR_T, token);
 			if (!node_token)
 				return (free(token), NULL);
 			ft_token_add_back(&head, node_token);
-			i = current_pos - cmd;
+			cmd++;
+			len = 0;
+			start_pos = cmd;
+			continue ;
 		}
-		if (cmd[i] == '|')
+		if (*cmd == '|')
 		{
-			token = ft_strdup("|");
-			if (!token)
-				return (free(token), NULL);
-			node_token = init_token(PIPE_T, token);
+			node_token = init_token(PIPE_T, NULL);
 			if (!node_token)
 				return (free(token), NULL);
 			ft_token_add_back(&head, node_token);
-			i++;
+			cmd++;
+			len = 0;
+			start_pos = cmd;
+			continue ;
 		}
-		else
+		if (*cmd == '>' && ft_strncmp(cmd, ">>", 2) != 0)
 		{
-			i++;
+			node_token = init_token(REDIR_F_T, NULL);
+			if (!node_token)
+				return (free(token), NULL);
+			ft_token_add_back(&head, node_token);
+			cmd++;
+			len = 0;
+			start_pos = cmd;
+			continue ;
 		}
+		if (*cmd == '<' && ft_strncmp(cmd, "<<", 2) != 0)
+		{
+			node_token = init_token(REDIR_B_T, NULL);
+			if (!node_token)
+				return (free(token), NULL);
+			ft_token_add_back(&head, node_token);
+			cmd++;
+			len = 0;
+			start_pos = cmd;
+			continue ;
+		}
+		len++;
+		cmd++;
 	}
 	return (head);
 }
+
 void print_token_list(t_token *head)
 {
     t_token *current = head;
     int count = 0;
-    
+
     if (!head)
     {
         printf("Empty token list\n");
         return;
     }
-    
+
     printf("TOKEN LIST:\n");
     while (current)
     {
