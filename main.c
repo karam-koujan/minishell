35f175f4707c8e91c887e83a933edaa15c5cb477
karam-koujan <karamkaku2000@gmail.com>
2025-03-30 18:52:19 +0000

    chore: uncomplete parser

--- main.c
+++ main.c
@@ -1,31 +1,124 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kkoujan <kkoujan@student.1337.ma>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/04 14:08:27 by kkoujan           #+#    #+#             */
-/*   Updated: 2025/03/30 18:49:55 by kkoujan          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "./includes/minishell.h"
+#include <stdio.h>
 
+#include <stdio.h>
 
+void print_cmd_table(t_cmd_table *cmd_table)
+{
+    int i;
+    int j;
+    t_redirection *redir;
+
+    if (!cmd_table)
+    {
+        printf("here Command table is NULL\n");
+        return;
+    }
+    
+    printf("Command Table:\n");
+    
+    if (cmd_table->cmd_count == 0)
+    {
+        printf(" here No commands in table\n");
+        return;
+    }
+    
+    i = 0;
+    while (i < cmd_table->cmd_count)
+    {
+        printf("Command %d:\n", i + 1);
+        
+        if (!cmd_table->cmds[i])
+        {
+            printf("  Command is NULL\n");
+            i++;
+            continue;
+        }
+        
+        // Print arguments
+        printf("  Arguments: ");
+        if (cmd_table->cmds[i]->argc == 0 || !cmd_table->cmds[i]->args)
+        {
+            printf("None\n");
+        }
+        else
+        {
+            j = 0;
+            while (j < cmd_table->cmds[i]->argc)
+            {
+                if (j > 0)
+                    printf(" ");
+                printf("'%s'", cmd_table->cmds[i]->args[j]);
+                j++;
+            }
+            printf("\n");
+        }
+        
+        // Print redirections
+        printf("  Redirections: ");
+        if (!cmd_table->cmds[i]->redirs)
+        {
+            printf("None\n");
+        }
+        else
+        {
+            printf("\n");
+            
+            redir = cmd_table->cmds[i]->redirs;
+            while (redir)
+            {
+                printf("    ");
+                
+                // Print redirection type
+                if (redir->type == REDIR_IN)
+                    printf("< ");
+                else if (redir->type == REDIR_OUT)
+                    printf("> ");
+                else if (redir->type == REDIR_APPEND)
+                    printf(">> ");
+                else if (redir->type == REDIR_HEREDOC)
+                    printf("<< ");
+                
+                // Print file or delimiter
+                if (!redir)
+                {
+                    printf("Redirection node is NULL\n");
+                     return;
+                }
+
+                if (redir->file_or_delimiter)
+                    printf("%s\n", redir->file_or_delimiter);
+                else
+                    printf("(NULL)\n");  // To debug i                
+                redir = redir->next;
+            }
+        }
+        
+        i++;
+    }
+}
 int main()
 {
-	char    *line;
+	char    *cmd;
+	t_token	*token_head;
+	t_cmd_table *cmd_table;
 	while (1337)
 	{
-	line = readline("minishell$ ");
-	if (!line)
+	cmd = readline("minishell$ ");
+	if (!cmd || !*cmd)
 		return (0);
-	printf("line %s\n", line);
-	printf("line %i\n", has_unclosed_quotes(line));
-	printf("line %i\n", has_misplaced_pipes(line));
-	printf("redir %i\n", has_invalid_redir(line));
-
-	free(line);
+    add_history(cmd);
+	if (!syntax_error(cmd))
+    {
+        continue;
+        free(cmd);
+    }
+	token_head = tokenize(cmd);
+	print_token_list(token_head);
+	cmd_table = parse(token_head);
+	print_cmd_table(cmd_table);
+	add_history(cmd);
+	free(cmd);
+	ft_token_lstclear(&token_head, free);
 	}
 }
