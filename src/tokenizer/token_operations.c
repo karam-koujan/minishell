ab8a231b2974126ab7234d1e22a851e3d40888ed
Karam Koujan <kkoujan@e1r8p2.1337.ma>
2025-05-05 11:17:41 +0100

    norm

--- src/tokenizer/token_operations.c
+++ src/tokenizer/token_operations.c
@@ -6,7 +6,7 @@
 /*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 20:07:49 by kkoujan           #+#    #+#             */
-/*   Updated: 2025/05/05 10:05:03 by kkoujan          ###   ########.fr       */
+/*   Updated: 2025/05/05 11:17:26 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,20 +47,63 @@ void	ft_token_add_back(t_token **lst, t_token *new)
 	}
 }
 
+void	loop_over_var(t_token **prev, t_token **curr, t_token **next, \
+	char **arr)
+{
+	*prev = (*curr)->next;
+	*next = init_token(WORD_T, ft_strdup(*arr));
+	if (!(*next))
+		return ;
+	(*curr)->next = *next;
+	*curr = *next;
+	if (*arr + 1 != NULL)
+	{
+		*next = init_token(SP_T, NULL);
+		if (!(*next))
+			return ;
+		(*curr)->next = *next;
+		*curr = *next;
+	}
+	(*curr)->next = *prev;
+}
+
+void	insert_var(t_token*tokenlst, char **arr)
+{
+	int		i;
+	t_token	*next;
+	t_token	*curr;
+	t_token	*prev;
+
+	curr = tokenlst;
+	next = NULL;
+	prev = NULL;
+	i = 0;
+	curr->type = WORD_T;
+	free(curr->val);
+	curr->val = ft_strdup(arr[0]);
+	if (!curr->val)
+		return ;
+	if (arr[1] == NULL)
+		return ;
+	next = init_token(SP_T, NULL);
+	if (!next)
+		return ;
+	next->next = curr->next;
+	curr->next = next;
+	curr = next;
+	while (arr[++i])
+		loop_over_var(&prev, &curr, &next, arr + i);
+}
+
 t_token	*handle_expand_var(t_token *tokenlst, t_env *env, int in_quote)
 {
 	char	*val;
 	char	**arr;
-	int		i;
-	t_token	*next;
-	t_token *curr;
-	t_token	*prev;
+	t_token	*curr;
 
-	prev = tokenlst;
 	curr = tokenlst;
 	if (!curr)
-		return (NULL);	
-	i = -1;
+		return (NULL);
 	val = ft_getenv_val(env, curr->val);
 	if (in_quote || ft_strlen(val) == 0)
 	{
@@ -69,47 +112,14 @@ t_token	*handle_expand_var(t_token *tokenlst, t_env *env, int in_quote)
 		tokenlst->val = val;
 		return (tokenlst->next);
 	}
-
 	arr = ft_split(val, ' ');
-	if (arr)
-	{
-		tokenlst->type = WORD_T;
-		free(tokenlst->val);
-		tokenlst->val = ft_strdup(arr[0]);
-		i++;
-		if (arr[1] == NULL)
-			return (free_arr(arr), tokenlst->next);
-		next = init_token(SP_T, NULL);
-		if (!next)
-			return (free_arr(arr), NULL);
-		next->next = tokenlst->next;
-		curr->next = next;
-		curr = next;
-	}
-	else
-		return (free(val), tokenlst->next);
-	while (arr[++i])
-	{
-		if (i == 0)
-			continue ;
-		prev = curr->next;
-		next = init_token(WORD_T, ft_strdup(arr[i]));
-		if (!next)
-			return (free_arr(arr), NULL);
-		curr->next = next;
-		curr = next;
-		if (arr[i + 1] != NULL)
-		{
-			next = init_token(SP_T, NULL);
-			if (!next)
-				return (free_arr(arr), NULL);
-			curr->next = next;
-			curr = next;
-		}
-		curr->next = prev;
-	}
+	if (arr == NULL)
+		return (NULL);
+	if (curr)
+		insert_var(curr, arr);
 	return (free_arr(arr), tokenlst->next);
 }
+
 void	join_var(t_token **tokenlst, t_env *env)
 {
 	t_token	*lst;
