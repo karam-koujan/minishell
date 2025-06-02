450540d615ab43ef54ad86ec83cd1c86fe1c899d
Karam Koujan <kkoujan@e1r8p13.1337.ma>
2025-06-02 12:17:43 +0100

    push

--- test.py
+++
@@ -1,8 +0,0 @@
-# Generating a pipeline of 6000 'ls' commands separated by pipes
-num_commands = 9000
-command = "ls"
-pipeline = " | ".join([command] * num_commands)
-
-# Writing the pipeline to a file
-with open("pipeline_test.txt", "w") as f:
-    f.write(pipeline)
